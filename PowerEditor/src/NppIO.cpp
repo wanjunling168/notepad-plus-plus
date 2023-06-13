wanjunling168/notepad-plus-plus

commit 5e2f5d79aa8325a3db9c3b811d80e9b7ce42b51c

Code enhancement: correct parameter error to call of doOpen
Fix #13150, close #13158

PowerEditor/src/NppIO.cpp
@@ -1991,7 +1991,7 @@ void Notepad_plus::fileOpen()
 	size_t sz = fns.size();
 	for (size_t i = 0 ; i < sz ; ++i)
 	{
-		BufferID test = doOpen(fns.at(i).c_str(), fDlg.isReadOnly());
+		BufferID test = doOpen(fns.at(i).c_str(), false, fDlg.isReadOnly());
 		if (test != BUFFER_INVALID)
 			lastOpened = test;
 	}

commit a266bd51710b2fe63f3d3d2e111bd99f710040ff

Fix defective detection for file read-only attribute removal via Explorer
The regression has been made by 5e2f5d79aa8325a3db9c3b811d80e9b7ce42b51c

Fix #13742

PowerEditor/src/NppIO.cpp
@@ -1998,7 +1998,7 @@ void Notepad_plus::fileOpen()
 	size_t sz = fns.size();
 	for (size_t i = 0 ; i < sz ; ++i)
 	{
-		BufferID test = doOpen(fns.at(i).c_str(), false, fDlg.isReadOnly());
+		BufferID test = doOpen(fns.at(i).c_str());
 		if (test != BUFFER_INVALID)
 			lastOpened = test;
 	}