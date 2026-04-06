f9073705895ab3242cb3ae31941e40a36b6bf7c5
Don Ho <don.h@free.fr>
2026-04-06 03:59:22 +0200

    Fix floating dialog not display content issue
    Fix #17563, close #17937

--- PowerEditor/src/WinControls/DockingWnd/DockingManager.cpp
+++ PowerEditor/src/WinControls/DockingWnd/DockingManager.cpp
@@ -675,11 +675,19 @@ void DockingManager::createDockableDlg(DockedWidgetData data, int iCont, bool is
 		}
 	}
 
-	// notify client app
+	// notify client zone
 	if (iCont < DOCKCONT_MAX)
 		SendNotify(data.hClient, MAKELONG(DMN_DOCK, iCont));
 	else
+	{
 		SendNotify(data.hClient, MAKELONG(DMN_FLOAT, iCont));
+
+		// Startup restore of a floating panel may skip WM_SIZE for this dialog.
+		// Force one layout pass so the list control gets a valid size.
+		RECT rc{};
+		::GetClientRect(data.hClient, &rc);
+		::SendMessage(data.hClient, WM_SIZE, 0, MAKELPARAM(rc.right - rc.left, rc.bottom - rc.top));
+	}
 }
 
 void DockingManager::setActiveTab(int iCont, int iItem)
